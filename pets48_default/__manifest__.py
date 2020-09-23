a9f6cb49d7145de58f4d6bebd7202e4d5f8f3d02
Gabriela Rivero <regaby@gmail.com>
2020-09-23 16:22:12 -0300

    [ADD] pets48_default module

---
+++ pets48_default/__manifest__.py
@@ -0,0 +1,46 @@
+{
+    'name': 'pets48',
+    'version': '13.0.0.0',
+    'category': 'Tools',
+    'summary': "Proyecto PETS48",
+    'author': 'Ing. Gabriela Rivero',
+    'depends': [
+        'base',
+    ],
+    'data': [
+    ],
+    'installable': True,
+    'application': False,
+
+    'limit_request': '8196',
+    'limit_memory_soft': '640000000',
+    'limit_memory_hard': '760000000',
+    'limit_time_cpu': '60',
+    'limit_time_real': '120',
+
+    # manifest version, if omitted it is backward compatible
+    'env-ver': '2',
+
+    # if Enterprise it installs in a different directory than community
+    'odoo-license': 'CE',
+
+    # port where odoo starts serving pages
+    'port': '8069',
+
+    # list of url repos to install in the form 'repo-url directory'
+    'git-repos': [
+        'https://github.com/regaby/cl-l10n_adhoc.git',
+        'https://github.com/regaby/odoo-custom.git',
+        ## localización
+        'https://github.com/ingadhoc/odoo-argentina.git',
+        'https://github.com/ingadhoc/odoo-argentina-ce.git',
+        'https://github.com/ingadhoc/argentina-sale.git',
+        'https://github.com/OCA/account-financial-reporting.git',
+    ],
+
+    'docker-images': [
+        'odoo regaby/odoo-ce:13.0',
+        'postgres postgres:10.1-alpine',
+        'nginx nginx'
+    ]
+}
